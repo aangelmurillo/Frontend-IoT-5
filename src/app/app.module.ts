4cc1e699239a0899d1f436fb2937cf19cdc664d1
Angel Roberto Ibañez Chavez <angelroberto110211@gmail.com>
2024-07-01 13:57:02 -0600

    Añadiendo archivos de ruta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
@@ -19,6 +21,7 @@ import { MatInputModule } from '@angular/material/input';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatFormFieldModule,
